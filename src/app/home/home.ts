58961ab4ede00bbf8ac08923e5ad02045da633a9
Babli Sharma <bablithv101@gmail.com.com>
2026-02-04 17:38:17 +0530

    some change in home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -9,11 +9,12 @@ import {
   CUSTOM_ELEMENTS_SCHEMA
 } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
