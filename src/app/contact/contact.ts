2ab69b03ce83857de32001392fe0ed506903eca0
Babli Sharma <bablithv101@gmail.com.com>
2026-02-25 16:13:55 +0530

    update contant compontent

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule], // ✅ sirf UI modules
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
@@ -23,13 +24,13 @@ export class Contact implements OnInit {
 
   ngOnInit(): void {
     this.contactForm = this.fb.group({
-      fullName: ['', Validators.required],
-      country: [''],
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      country: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      phone: [''],
+      phone: ['', Validators.pattern('^[0-9]{7,15}$')],
       company: [''],
-      subject: [''],
-      message: ['']
+      subject: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(10)]],
     });
   }
 
@@ -41,19 +42,26 @@ export class Contact implements OnInit {
 
     this.loading = true;
 
-    this.http.post('https://vtc-database.vercel.app/api/contact', this.contactForm.value)
-      .subscribe({
-        next: () => {
-          this.successMsg = 'Message sent successfully ✅';
-          this.errorMsg = '';
-          this.contactForm.reset();
-          this.loading = false;
-        },
-        error: () => {
-          this.errorMsg = 'Something went wrong ❌';
-          this.successMsg = '';
-          this.loading = false;
-        }
-      });
+    this.http.post(
+      'https://vtc-database.vercel.app/api/contact',
+      this.contactForm.value
+    ).subscribe({
+      next: () => {
+        this.successMsg = 'Message sent successfully ✅';
+        this.errorMsg = '';
+        this.contactForm.reset();
+        this.loading = false;
+      },
+      error: () => {
+        this.errorMsg = 'Something went wrong ❌';
+        this.successMsg = '';
+        this.loading = false;
+      }
+    });
+  }
+
+  // 👇 shortcut for template
+  f(control: string) {
+    return this.contactForm.get(control);
   }
 }
